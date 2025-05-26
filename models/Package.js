32f4f8d26f71b7c8e236e84bbd066430b248a8ae
florent <florentpop@gmail.com>
2025-05-26 18:37:15 +0000

    package model editted

--- models/Package.js
+++ models/Package.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const PackageSchema = new Schema({
   name: { type: String, required: true },
   active: { type: Boolean, default: true },
-  duration: { type: String, required: true },
+  duration: { type: String, default: 'Non-expire' },
   price: { type: Number, required: true, min: 0 }
 }, { timestamps: true });
 
