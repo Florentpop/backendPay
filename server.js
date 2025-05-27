91ff8dce2f650f561de1883ef8ad193e8984af32
florent <florentpop@gmail.com>
2025-05-27 15:58:55 +0000

    Add flosel.hub to CORS

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost.com','https://hotspot-backend-dlwy.onrender.com','http://127.0.0.1:5500']
+  origin: ['http://localhost.com','https://hotspot-backend-dlwy.onrender.com','http://127.0.0.1:5500', 'http://flosel.hub'],
+  credentials: true
 }));
 app.use(bodyParser.json());
 
