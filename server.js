29045be2bd4a44d75c81d2c3a5743a0000789d50
florent <florentpop@gmail.com>
2025-10-25 20:25:43 +0000

    Update CORS configuration: add 'http://127.0.0.1:5500' to allowed origins for local development.

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://hotspot-admin-dashboard.onrender.com','https://hotspot-backend-dlwy.onrender.com','http://localhost:5173'],
+  origin: ['https://hotspot-admin-dashboard.onrender.com','https://hotspot-backend-dlwy.onrender.com','http://127.0.0.1:5500','http://localhost:5173'],
   credentials: true
 }));
 app.use(bodyParser.json());
